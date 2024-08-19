ee95c98ec07d6d05b7d0802f1a0c5b57cd41f2f8
BobinaNo <lentabobina@gmail.com>
2024-08-20 00:40:26 +0300

    feat:working ver

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const bracketList = {};
+
+  for(let[open,close] of bracketsConfig){
+    bracketList[open] = close;
+  }
+
+  const order = [];
+
+  for (let char of str){
+    if(bracketList[char] === char){
+      if(order[order.length-1] === char){
+        order.pop();
+      } else {
+      order.push(char);
+    }
+  } else if (bracketList[char]){
+    order.push(char)
+  } else {
+    const closeChar = order.pop();
+    if(bracketList[closeChar] !==char){
+      return false;
+    }
+  }
+}
+  return order.length === 0;
+ 
 }
